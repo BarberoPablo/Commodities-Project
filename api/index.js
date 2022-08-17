2e93339596595dcce5b5d1515474838f6958e531
Maximiliano Marolda <mmarolda12@gmail.com>
2022-08-17 15:29:14 -0300

    feat(api/index AUTH0): add authenticaction method

--- api/index.js
+++ api/index.js
@@ -19,10 +19,33 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || 3000;
+require('dotenv').config();
+const { auth, requiresAuth } = require('express-openid-connect');
+
+server.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL ,
+    baseURL: process.env.BASE_URL ,
+    clientID: process.env.CLIENT_ID ,
+    secret: process.env.SECRET ,
+    idpLogout: true,
+  })
+);
+
+server.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged Out")
+});
+
+server.get('/profile', requiresAuth(), (req, res) => {  //This route reauieres to be authenticated to be able to get it
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+//conn.sync({ force: true }).then(() => {  // COMENTADO PARA PODER LEVANTAR LA APP SIN DB
+  server.listen(port, () => {
+    console.log(`Server listening at Port ${port}`); // eslint-disable-line no-console
   });
-});
+//});
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { auth, requiresAuth } = require('express-openid-connect'); // se usa con el middleware para securizar las rutas que requieren autenticación
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
