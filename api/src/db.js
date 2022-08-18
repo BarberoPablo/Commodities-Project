6b2d88235d0e0852ae05807cd11a3145273472f6
Maximiliano Marolda <mmarolda12@gmail.com>
2022-08-18 17:15:14 -0300

    Feat(api/db.js)Creates relation between models

--- api/src/db.js
+++ api/src/db.js
@@ -30,17 +30,36 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { User, Category, Plan, Post, SubCategory } = sequelize.models;
+const { User, Category, Plan, Post, SubCategory, Review, ReviewUser, Feedback } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 Category.hasMany(Post);  //hasOne para cuando es 1:1
 Post.belongsTo(Category);
 
+SubCategory.hasMany(Post);
+Post.belongsTo(SubCategory);
+
 Category.hasMany(SubCategory);
 SubCategory.belongsTo(Category);
 
+User.hasMany(Post);
+Post.belongsTo(User);
 
+Plan.hasMany(User);
+User.belongsTo(Plan);
+
+User.hasMany(Feedback);
+Feedback.belongsTo(User);
+
+User.hasMany(Review);
+Review.belongsTo(User);
+
+User.hasOne(ReviewUser);
+ReviewUser.belongsTo(User);
+
+ReviewUser.hasMany(Review);
+Review.belongsTo(ReviewUser);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
