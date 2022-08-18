92a5e853265747567390e2495187ffdb4830a678
lbenjaminq <benjaminlbq12@gmail.com>
2022-08-18 17:49:12 -0300

    Feat(client/App.js):Add route Home

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux'
 import store from './Redux/store'
-
+import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
   <Provider store={store} >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
