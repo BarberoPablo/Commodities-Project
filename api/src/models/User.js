d55bf3c1bedab4b438515db62511b47002b78add
Pablo Barbero <pablobarbero220@gmail.com>
2022-08-18 13:19:00 -0300

    Feat(api/models): user model

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -8,5 +8,45 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    isSeller: { //isBuyer no lo puse
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING, //STRING
+      allowNull: true, //true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,//true
+    },
+    verified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false, //nuevo 
+    },
+    country: {
+      type: DataTypes.STRING, //ENUM ??
+      allowNull: false,
+    },
+    shipping: {
+      type: DataTypes.BOOLEAN,//ENUM con CIF y FOB
+      allowNull: false,
+    },
+    //idPlan foreign key
+    contactsId: {
+      type: DataTypes.ARRAY(DataTypes.STRING), //se guardan los id de los match
+    },
+    remainingContacts: {
+      type: DataTypes.INTEGER,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    isBanned: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+
   });
 };
