1d21ea52bbee47252e4318ca505dda04fbf11ab9
CesarJelvez <cesarjelvez13@gmail.com>
2022-08-19 19:13:59 -0300

    Feat (api/routes): Post Category

--- api/src/routes/router.js
+++ api/src/routes/router.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserDetails, getPosts, createPost, getCategory, getSubCategory } = require("./services");
+const { getUserDetails, getPosts, createPost, getCategory, getSubCategory, postCategory } = require("./services");
 
 router.get("/userDetails/:id", getUserDetails);
 
 router.get("/posts/:idPost", getPosts);
 
-router.get("/review/:id", getReviews);
+//router.get("/review/:id", getReviews);
 
 router.post("/post/:id", createPost);
 
 router.get("/category", getCategory);
 
 router.get("/subCategory/:id", getSubCategory);
+
+router.post("/category", postCategory)
 
 module.exports = { router };
