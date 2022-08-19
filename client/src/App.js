d7ffe368c6e3594126c8de40e49211b3cc80032c
intelligentdnb <juandiarts@gmail.com>
2022-08-18 21:43:16 -0300

    refactor/(client/src/App.js): adding the routes of the footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 import Home from './Components/Home/Home';
 // import { getPost,getUser } from './Redux/Actions/Actions'; action para hacer el dispatch y treaer users,posts,etc
 import {Route} from 'react-router-dom'
+import Footer from "./Components/Home/Footer/Footer.jsx"
+import AboutUs from "./Components/Home/Footer/AboutUs.jsx"
+import ContactUs from "./Components/Home/Footer/ContactUs.jsx"
+import PrivacyPolicy from "./Components/Home/Footer/PrivacyPolicy.jsx"
+import TermsOfUse from "./Components/Home/Footer/TermsOfUse.jsx"
+
 
 function App() {
 
   return (
     <div className="App">
       <Route exact path={'/'} component={Home}/>
+      <Route path={'/about-us'} component={AboutUs}/>
+      <Route path={'/contact-us'} component={ContactUs}/>
+      <Route path={'/privacy-policy'} component={PrivacyPolicy}/>
+      <Route path={'/terms-of-use'} component={TermsOfUse}/>
+      <Route path={'/'} component={Footer}/>
       {/* More routes eje: Profile, Post, UserProfile  */}
     </div>
   );
