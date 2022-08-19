f1296769e208ca38773d4f3e1815655e019abb50
Maximiliano Marolda <mmarolda12@gmail.com>
2022-08-19 18:44:06 -0300

    feat(api/routes/services.js & router.js):added new route

--- api/src/routes/router.js
+++ api/src/routes/router.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserDetails, createPost, getCategory } = require("./services");
+const { getUserDetails, createPost, getCategory, getReview, createReview } = require("./services");
 
 router.get("/userDetails/:id", getUserDetails);
 
-router.get("/review/:id", getReviews);
+router.get("/review/:id", getReview);
 router.post("/post/:id", createPost);
 
 router.get("/category", getCategory);
+router.post("/review/", createReview);
+
+
+
 
 module.exports = { router };
