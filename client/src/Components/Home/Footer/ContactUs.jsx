aad12203bb74d0c49c49c94b0c6937c218733fa8
intelligentdnb <juandiarts@gmail.com>
2022-08-19 00:09:44 -0300

    footer/not to push yet

--- client/src/Components/Home/Footer/ContactUs.jsx
+++ client/src/Components/Home/Footer/ContactUs.jsx
@@ -4,6 +4,9 @@ const ContactUs = () => {
   return (
     <div>
       <h1>Have a question about our services? Need Help?</h1>
+      <a href="/feedback">📧Click here to email us.</a>
+      <p>📞Call us at +54 9 3764 410695</p>
+      <h5>Send us a quick email and we will be glad to help you. You can contact us at the following address:</h5>
     </div>
   )
 }
