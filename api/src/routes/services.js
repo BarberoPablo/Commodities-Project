16efe65c89bc3c6be825e76416aef35f1e1bbac8
Pablo Barbero <103137619+BarberoPablo@users.noreply.github.com>
2022-08-19 13:56:22 -0300

    Merge services

--- api/src/routes/services.js
+++ api/src/routes/services.js
@@ -154,6 +154,44 @@ const createPost = async(req, res)=>{
   }
 }
 
+const getReview = async(req, res)=>{
+  try {
+   const {id} = req.params;
+   let review = await b2b.findOne({
+    where: {id: id}
+   });
+   return review;
+  } catch (error) {
+    console.log (error)
+  }
+};
 
+const getCategory = async (req , res) => {
+  try {
+    let category = await Category.findAll({
+      include: SubCategory
+     });
 
-module.exports = {getUserDetails, createPost, getPosts}
+     res.status(200).send(category)
+
+  } catch (error) {
+    res.status(404).send(error)
+  }
+}
+
+const getSubCategory = async (req , res) => {
+  try {
+    const { id } = req.params
+
+    let subCategory = await SubCategory.findAll({
+      where : {idCategory: id},
+    });
+
+    res.status(200).json(subCategory)
+    
+  } catch (error) {
+    res.status(404).send(subCategory)
+  }
+}
+
+module.exports = {getUserDetails , createPost, getPosts, getCategory, getReview , getSubCategory}
