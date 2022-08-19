e169a57f9f7987adf3a4bad9ff692352b59c3e3d
NicoMSL <nsanchezlong@gmail.com>
2022-08-19 11:47:08 -0300

    createPostUpdate

--- client/src/Components/CreatePost/CreatePost.jsx
+++ client/src/Components/CreatePost/CreatePost.jsx
@@ -1,27 +1,32 @@
-import React, { useEffect, useState } from "react";
+//<Route path="/create-post" component={CreatePost}/>
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 // import { postPost } from "../actions/index";  //importar acciones
 //import { useDispatch, useSelector } from "react-redux";
 
 export default function CreatePost() {
-  //const dispatch = useDispatch();
-  //const post = useSelector((state) => state.posts);   // importar state
+  const dispatch = useDispatch();
   const [errors, setErrors] = useState({})    //validaciones front
+  const [checkBS, setCheckBS] = useState(0) 
+
+  //actions necesarias
+  //post post(dispatch)
+  //get category y sub category
 
 const [input, setInput] = useState({ //acomadar a modelo
   id: "",
   user: "",
   tittle:"",
-  post: "",
-  Shippingmethods:"",
-  Paymentmethod:"",
-  buy: false,
+  description: "",
   sell: false,
+  shipping:"",
+  payment:"",
   Category: "",
   SubCategory: "",
   idUser:"",
+  image:"",
 });
-
+.
 //handles
 function handleChange(e){
   setInput({
@@ -30,15 +35,17 @@ function handleChange(e){
   })}
 
 function handleCheck(e){
+  setCheckBS(1)
   if(e.target.value === "sell"){
     setInput({
       ...input,
       sell:true
     })
-  } else{
+  } 
+  if(e.target.value === "buy"){
     setInput({
       ...input,
-      buy:true
+      sell:false
     })
   }
   }
@@ -47,33 +54,43 @@ function handleSubmit(e){
   e.preventDefault();
   let val = validacion(input);
   setErrors(val)
+  dispatch(postPost(input))
   if(Object.keys(val).length >0 ){
     alert("Fix errors");
     val = {}
     return
   }
   console.log(input)
-  //dispatch(postPost(input))
-  alert("Activity Created")
+  alert("Post Created")
   setInput({
     id: "",
     user: "",
     tittle:"",
-    post: "",
-    buy: false,
+    description: "",
     sell: false,
-    Shippingmethods:"",
+    shipping:"",
+    payment:"",
     Category: "",
     SubCategory: "",
     idUser:"",
+    image:"",
   })
 }
 
 //validacion
 function validacion(input){
-    let errors = {};
-    return errors
-  }
+  let errors = {};
+  if(!input.tittle){errors.tittle="Please complete the tittle of the post"}
+  if(input.description === ""){errors.description = "Complete description";}
+  if(input.description.length > 255){errors.description = "the description can not have more than 255 characters";}
+  if(input.shipping === ""){errors.shipping = "Complete shipping";}
+  if(input.payment === ""){errors.payment = "Complete payment";}
+  if(input.Category === ""){errors.Category = "Complete Category";}
+  //if(input.SubCategory === ""){errors.SubCategory = "Complete SubCategory";}
+  if(checkBS<1){errors.sell = "Select sell or buy";}
+
+  return errors
+}
 
   return (
 <div>
@@ -84,34 +101,44 @@ function validacion(input){
 <form onSubmit={(e)=>handleSubmit(e)}>
   <label>Tittle: </label>
   <input type="text" value={input.tittle} autoComplete="off" placeholder="Please write a tittle..." name="tittle" onChange={(e)=>handleChange(e)}/><br/>
-  
-  <select value={input.Shippingmethods} name="Shippingmethods" onChange={(e)=>handleChange(e)}>
+  {errors.tittle &&<p className="err">{errors.tittle}</p>}
+
+  <select value={input.shipping} name="shipping" onChange={(e)=>handleChange(e)}>
     <option hidden value="">Shipping method</option>
     <option value="CIF">CIF</option>
     <option value="FOB">FOB</option>
   </select>
+  {errors.shipping &&<p className="err">{errors.shipping}</p>}
 
-  <select value={input.Paymentmethod} name="Paymentmethod" onChange={(e)=>handleChange(e)}>
+  <select value={input.payment} name="payment" onChange={(e)=>handleChange(e)}>
     <option hidden value="">Payment method</option>
     <option value="LC">LC</option>
     <option value="DLC">DLC</option>
     <option value="SBLC">SBLC</option>
   </select><br/>
+  {errors.payment &&<p className="err">{errors.payment}</p>}
 
   <select value={input.Category} name="Category" onChange={(e)=>handleChange(e)}>
     <option hidden value="">Select category</option>
-    <option value="">category1</option>
+    <option value="asd">get categoriy</option>
   </select><br/>
+  {errors.Category &&<p className="err">{errors.Category}</p>}
 
-  <label>Post: </label>
-  <textarea value={input.post} autoComplete="off" placeholder="Please write a descrition..." name="post" onChange={(e)=>handleChange(e)}/><br/>
+  {/* <select value={input.Category} name="Category" onChange={(e)=>handleChange(e)}>
+    <option hidden value="">Select category</option>
+    <option value="">get categoriy</option>
+  </select><br/> */}
+
+  <label>Description: </label>
+  <textarea value={input.description} autoComplete="off" placeholder="Please write a description..." name="description" onChange={(e)=>handleChange(e)}/><br/>
+  {errors.description &&<p className="err">{errors.description}</p>}
 
   <label><input onChange={(e)=>handleCheck(e)}  type="radio" name="check" value="buy" />Buy</label>
   <label><input onChange={(e)=>handleCheck(e)}  type="radio" name="check" value="sell"/>sell</label><br/>
+  {errors.sell &&<p className="err">{errors.sell}</p>}
 
   <button type="submit" className="boton">Create Post</button>
 </form>
-
 </div>
   )}
 
