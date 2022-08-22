b7930a10c6cc9dbbb027a428c60d4037f5ca2d87
lbenjaminq <benjaminlbq12@gmail.com>
2022-08-22 11:14:06 -0300

    Feature(client/Search):Add search funcionality

--- client/src/Components/Home/Navbar/Search.jsx
+++ client/src/Components/Home/Navbar/Search.jsx
@@ -1,35 +1,37 @@
-import React from 'react';
-import { useState } from 'react';
-//import { useDispatch } from 'react-redux';
-//import { searchPosts } from '../../../Redux/Actions/Actions';
-import s from './Navbar.module.css'
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { searchPosts } from "../../../Redux/Actions/Actions";
 
 const Search = () => {
-  //const dispatch = useDispatch();
-  const [input,setInput] = useState('')
 
-  const handleChange = (e) =>{
+  const dispatch = useDispatch();
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
     setInput(e.target.value);
-  }
+  };
 
-    const handleClick = (e) =>{
-      if(e.target.innerHTML === 'Buyers'){
-        console.log('Buyers', input)
-  
-      }
-      if(e.target.innerHTML === 'Sellers'){
-        console.log('Sellers', input)
-        
-      }
+  const handleClick = (e) => {
+    let sell = false;
+
+    if (e.target.innerHTML === "Buyers" && input !== '') {
+      console.log("Buyers", input);
+      dispatch(searchPosts({ input, sell: true }));
     }
+    if (e.target.innerHTML === "Sellers" && input !== '') {
+      console.log("Sellers", input);
+      dispatch(searchPosts({ input, sell }));
+    }
+  };
 
   return (
-      <div>
-        <input onChange={handleChange} type='text' value={input} />
-        <button onClick={handleClick}>Buyers</button>
-        <button onClick={handleClick}>Sellers</button>
-      </div>
-  )
-}
+    <div>
+      <input onChange={handleChange} type="text" value={input} />
+      <button onClick={handleClick}>Buyers</button>
+      <button onClick={handleClick}>Sellers</button>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
