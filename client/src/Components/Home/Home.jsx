94fd5d72e4d771c7471d9e7aaffa2f7414c1eee5
Servezynski <servezynskimarcos1@gmail.com>
2022-08-25 13:16:56 -0300

    Feat(/client):add react formik

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -2,21 +2,24 @@ import {React, useEffect, useState }  from 'react'
 //import Navbar from './Navbar/Navbar'
 import DrawerCategories from './DrawerCategories/DrawerCategories'
 import {useDispatch, useSelector} from 'react-redux'
-import { getPost, getCategoriesByName} from '../../Redux/Actions/Actions'
+import { getPost, getCategoriesByName, getUserDetails} from '../../Redux/Actions/Actions'
 import Cards from './Card/Cards'
 import Paginado from "./Paginado/Paginado"
 import s from './Home.module.css'
 import Filters from './Filters/Filters'
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Home = ({currentPage, setCurrentPage}) => {
   const dispatch = useDispatch()
   const {allCategories} = useSelector(state => state.categories)
   const {posts} = useSelector(state=>state.posts)
-  
+  const { user } = useAuth0();
+
   useEffect(()=>{
+    dispatch(getUserDetails(user.email))
     dispatch(getPost())
     dispatch(getCategoriesByName())
-  },[dispatch])
+  },[dispatch, user.email])
 
 //paginado
  //pagina actual
@@ -34,6 +37,7 @@ const paginado = (pageNumber) => {
 
   return (
     <div>
+    
     <div className={s.container}>
       <DrawerCategories allCategories={allCategories} setCurrentPage={setCurrentPage}/>
       <Cards currentPost={currentPost} />
--- client/src/Redux/Actions/Actions.js
+++ client/src/Redux/Actions/Actions.js
@@ -1,66 +1,86 @@
-import { getAllPosts, getPosts, filteredSubcategory, getUserPosts, filteredPayment, filteredCountry, filteredShippment } from "../Slices/postsSlice";
-import { getAllUsers } from "../Slices/usersSlice";
+import {
+  getAllPosts,
+  getPosts,
+  filteredSubcategory,
+  getUserPosts,
+  filteredPayment,
+  filteredCountry,
+  filteredShippment,
+} from "../Slices/postsSlice";
+import { getUserDetail, getAllUsers, userLog } from "../Slices/usersSlice";
 import { getCategories } from "../Slices/categoriesSlice";
 import { getCountries } from "../Slices/countriesSlice";
 
-import axios from 'axios';
+import axios from "axios";
 
 // FUNCTIONS POSTS
 
-export const getPost = ()=>(dispatch)=>{
-  axios('https://b2b-01.herokuapp.com/posts') // end-point del back /posts
-  .then(data => dispatch(getAllPosts(data.data)))
-  .catch(e=>console.log(e))
-}
+export const getPost = () => (dispatch) => {
+  axios("https://b2b-01.herokuapp.com/posts") // end-point del back /posts
+    .then((data) => dispatch(getAllPosts(data.data)))
+    .catch((e) => console.log(e));
+};
 
-export const searchPosts = (name,sell) =>(dispatch)=>{
-  dispatch(getPosts(name,sell))
-}
+export const searchPosts = (name, sell) => (dispatch) => {
+  dispatch(getPosts(name, sell));
+};
 
-export const userPosts = () =>(dispatch)=>{
-  axios('/posts') // end-point del back /posts
-  .then(data => dispatch(getUserPosts(data.data)))}
+export const userPosts = () => (dispatch) => {
+  axios("https://b2b-01.herokuapp.com/posts") // end-point del back /posts
+    .then((data) => dispatch(getUserPosts(data.data)));
+};
 
 // FUNCTIONS CATEGORIES
 
 export const getCategoriesByName = () => (dispatch) => {
-  axios('https://b2b-01.herokuapp.com/category')
-  .then(resp => dispatch(getCategories(resp.data)))
-}
+  axios("https://b2b-01.herokuapp.com/category").then((resp) =>
+    dispatch(getCategories(resp.data))
+  );
+};
 
-// FILTERS SUBCATEGORY 
+// FILTERS SUBCATEGORY
 
-export const filterBySubcategory = (value) => (dispatch)=>{
-  dispatch(filteredSubcategory(value))
-}
+export const filterBySubcategory = (value) => (dispatch) => {
+  dispatch(filteredSubcategory(value));
+};
 
 // FILTER PAYMENT
 
 export const filterByPayment = (value) => (dispatch) => {
-  dispatch(filteredPayment(value))
-}
+  dispatch(filteredPayment(value));
+};
 
-//FILTER COUNTRY 
+//FILTER COUNTRY
 export const filterCountry = (value) => (dispatch) => {
-  dispatch(filteredCountry(value))
-}
+  dispatch(filteredCountry(value));
+};
 
 //FILTER SHIPPMENT
 export const filterShippment = (value) => (dispatch) => {
-  dispatch(filteredShippment(value))
-}
+  dispatch(filteredShippment(value));
+};
 
 // FUNCTIONS USERS
 
-export const getUser = ()=>(dispatch)=>{
-  axios('https://rickandmortyapi.com/api/character') // end-point del back /users
-  .then(data=>dispatch(getAllUsers(data.data.results)))
-  .catch(e=>console.log(e))
-}
+export const getUser = () => (dispatch) => {
+  axios("https://rickandmortyapi.com/api/character") // end-point del back /users
+    .then((data) => dispatch(getAllUsers(data.data.results)))
+    .catch((e) => console.log(e));
+};
 
 //get counties
-export const getAllCountries = ()=>(dispatch)=>{
-  axios('https://restcountries.com/v3/all') // end-point del back /users
-  .then(data=>dispatch(getCountries(data.data)))
-  .catch(e=>console.log(e))
-}
+export const getAllCountries = () => (dispatch) => {
+  axios("https://restcountries.com/v3/all") // end-point del back /users
+    .then((data) => dispatch(getCountries(data.data)))
+    .catch((e) => console.log(e));
+};
+
+export const userLogin = (payload) => (dispatch) => {
+  dispatch(userLog(payload));
+};
+
+export const getUserDetails = (email) => (dispatch) => {
+  axios("https://b2b-01.herokuapp.com/user/"+email)
+  .then((data) => dispatch(getUserDetail(data.data)))
+  .catch((e) => console.log(e));
+};
