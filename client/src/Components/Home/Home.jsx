ee0ce1a19487ffd67da07bd01a0545b645bd1654
lbenjaminq <benjaminlbq12@gmail.com>
2022-08-27 18:19:00 -0300

    Refactor(client/Home):Fixing bugs

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import { React, useEffect, useState } from "react";
-//import Navbar from './Navbar/Navbar'
 import DrawerCategories from "./DrawerCategories/DrawerCategories";
 import { useDispatch, useSelector } from "react-redux";
 import { getPost, getCategoriesByName, getUserDetails } from "../../Redux/Actions/Actions";
@@ -8,6 +7,8 @@ import Paginado from "./Paginado/Paginado";
 import s from "./Home.module.css";
 import Filters from "./Filters/Filters";
 import { useAuth0 } from "@auth0/auth0-react";
+import Container from 'react-bootstrap/esm/Container'
+
 
 const Home = ({ currentPage, setCurrentPage }) => {
   const product = {
@@ -30,7 +31,7 @@ const Home = ({ currentPage, setCurrentPage }) => {
 
   //paginado
   //pagina actual
-  const [postPerPage, setPostPerPage] = useState(3); //cantidad de cards por paginas
+  const [postPerPage, setPostPerPage] = useState(4); //cantidad de cards por paginas
   const indexOfLastPost = currentPage * postPerPage;
   const indexOfFirstPost = indexOfLastPost - postPerPage;
   const currentPost = posts.slice(indexOfFirstPost, indexOfLastPost);
@@ -43,8 +44,10 @@ const Home = ({ currentPage, setCurrentPage }) => {
     <div>
       <div className={s.container}>
         <DrawerCategories allCategories={allCategories} setCurrentPage={setCurrentPage} />
-        <Cards currentPost={currentPost} />
-        <Filters setCurrentPage={setCurrentPage} />
+        <Container fluid>
+          <Filters setCurrentPage={setCurrentPage} />
+          <Cards currentPost={currentPost} />
+        </Container>
       </div>
       {}
       <Paginado
