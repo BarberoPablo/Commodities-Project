1415b8993391dcbdb26de3a54b8243ff2f83efff
Servezynski <servezynskimarcos1@gmail.com>
2022-08-27 17:19:33 -0300

    fix(/client)solution fix user

--- client/src/Components/Home/Card/Cards.jsx
+++ client/src/Components/Home/Card/Cards.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import CardDetail from "./CardDetail";
 import s from "./Card.module.css"
+import { useSelector } from "react-redux";
 
 const Cards = ({ currentPost }) => {
+  const { allUsers } = useSelector((state) => state.users);
+
   return (
     <div className={s.container}>
       {currentPost?.map((e, i) => {
-        return <CardDetail e={e} key={i} />;
+        return <CardDetail e={e} key={i} user={allUsers.find((c) => e.userId === c.id)}/>;
       })}
     </div>
   );
