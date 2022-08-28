2428fcb413de01e06615ca8ce593b2e3d473c151
Servezynski <servezynskimarcos1@gmail.com>
2022-08-28 17:08:00 -0300

    fix(/client): resolve conflicts form

--- client/src/Components/Home/Card/CardDetail.jsx
+++ client/src/Components/Home/Card/CardDetail.jsx
@@ -3,9 +3,13 @@ const CardDetail = ({ e, user }) => {
   return (
     <div className={s.card}>
       <div className={s.userDate}>
-        <div className={s.imgProfile} >
+        <div className={s.imgProfile}>
           <img
-            src={user?.image ? user?.image : "https://campussafetyconference.com/wp-content/uploads/2020/08/iStock-476085198.jpg"}
+            src={
+              user?.image
+                ? user?.image
+                : "https://campussafetyconference.com/wp-content/uploads/2020/08/iStock-476085198.jpg"
+            }
             alt="profile"
           />
         </div>
@@ -18,11 +22,25 @@ const CardDetail = ({ e, user }) => {
       </div>
       <div className={s.post}>
         <div className={s.info}>
-          <p>Category: <b>{e.categoryName}</b></p>
-          <p>Sub Category: <b>{e.subCategory}</b></p>
-          <p>Country: <b>{e.country}</b></p>
-          <p>Payment: <b>{e.payment}</b></p>
-          <p>Shipping: <b>{e.shipping}</b></p>
+          <p>
+            Category: <b>{e.categoryName}</b>
+          </p>
+          <p>
+            Sub Category: <b>{e.subCategory}</b>
+          </p>
+          <p>
+            Country: <b>{e.country}</b>
+          </p>
+          <p>
+            payment:{" "}
+            {e.payment?.map((e) => {
+              return <b>{e} </b>;
+            })}
+          </p>
+
+          <p>
+            Shipping: <b>{e.shipping}</b>
+          </p>
         </div>
         <hr />
         <b>{e.title}</b>
