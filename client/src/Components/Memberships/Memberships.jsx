8258631366d808707ac0339768a4e2d37320d15f
Pablo Barbero <pablobarbero220@gmail.com>
2022-08-27 22:42:10 -0300

    test 01

--- client/src/Components/Memberships/Memberships.jsx
+++ client/src/Components/Memberships/Memberships.jsx
@@ -2,23 +2,33 @@ import React, { useEffect } from "react";
 import Plan from "./Plan";
 import { useDispatch, useSelector } from "react-redux";
 import { getPlans } from "../../Redux/Actions/Actions";
+import { getUserDetails } from "../../Redux/Actions/Actions";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Memberships = () => {
   const { memberships } = useSelector((state) => state.plans);
   const dispatch = useDispatch();
+  const { user } = useAuth0();
 
   useEffect(() => {
     dispatch(getPlans());
-  }, [dispatch]);
+    // Tal vez no sea necesario preguntar por el user:
+    if (user) {
+      dispatch(getUserDetails(user.email));
+    }
+    console.log("1:", user);
+    //Sacar user:
+  }, [dispatch, user]);
   /* Estructura de un plan:
-    name: STRING,
-    cost: INTEGER,
-    contacts: INTEGER,
-    posts: BOOLEAN
-    reviews: BOOLEAN,
-*/
+  name: STRING,
+  cost: INTEGER,
+  contacts: INTEGER,
+  posts: BOOLEAN
+  reviews: BOOLEAN,
+  */
   return (
     <div>
+      {console.log("1:", user)}
       {console.log(memberships)}
       {memberships?.map((plan, index) => {
         return (
