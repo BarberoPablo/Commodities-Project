c2ddfebb58d1e3047dd8e02593e66a1e64ec83e8
intelligentdnb <juandiarts@gmail.com>
2022-08-29 19:57:15 -0300

    fix/filterCountrys

--- client/src/Redux/Slices/countriesSlice.js
+++ client/src/Redux/Slices/countriesSlice.js
@@ -4,13 +4,29 @@ export const countriesSlice = createSlice({
   name: "countries",
   initialState: {
     allCountries: [],
+    countriesSorted: [],
   },
   reducers: {
     getCountries: (state, action) => {
       state.allCountries = action.payload;
     },
+    sortCountries: (state, action) => {
+    const countrysWithPost = action.payload.map((e) => e.country).sort();
+    const countrysSet = new Set(countrysWithPost);
+    const countrysToMap = [];
+    countrysSet.forEach((e) => {
+      let point = "";
+      if (e === "") {
+        countrysSet.delete(point);
+      }
+    });
+    countrysSet.forEach((e) => {
+      countrysToMap.push(e);
+    });
+    state.countriesSorted = countrysToMap;
+    }
   },
 });
 
-export const { getCountries } = countriesSlice.actions;
+export const { getCountries, sortCountries } = countriesSlice.actions;
 export default countriesSlice.reducer;
