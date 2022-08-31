ca512b9f6c17a3c576901c9b856a16494276091f
lbenjaminq <benjaminlbq12@gmail.com>
2022-08-31 18:23:20 -0300

    Refactor(client):Fixing footerVH

--- client/src/Components/Home/Footer/ContactUs.jsx
+++ client/src/Components/Home/Footer/ContactUs.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import BahiaBlanca from './Maps/BahiaBlanca'
-import PuertoMadero from './Maps/PuertoMadero'
-import Posadas from './Maps/Posadas'
-import s from './css/Contact.module.css'
+import React from "react";
+import BahiaBlanca from "./Maps/BahiaBlanca";
+import PuertoMadero from "./Maps/PuertoMadero";
+import Posadas from "./Maps/Posadas";
+import s from "./css/Contact.module.css";
+
 const ContactUs = () => {
   return (
     <div className={s.container}>
       <h1>Have a question about our services? Need Help?</h1>
       {/*<a href="/feedback">📧Click here to email us.</a>*/}
-      <h5>Send us a quick email and we will be glad to help you. You can contact us at the following address:</h5>
-      <p>📧<a href='/feedback'>commoditiesb2b@hotmail.com</a></p>
+      <h5>
+        Send us a quick email and we will be glad to help you. You can contact
+        us at the following address:
+      </h5>
+      <p>
+        📧<a href="/feedback">commoditiesb2b@hotmail.com</a>
+      </p>
       <div>
         <address>
           <span>Av. Colón 22</span> <br />
@@ -17,29 +23,35 @@ const ContactUs = () => {
           <span>Buenos Aires</span>, <br />
           <span>8000 -</span> <span> ARG</span>. <br />
           Phone: <span>+54 9 2916 41-9871</span> <br />
-          <div className={s.map}><BahiaBlanca/></div>
+          <div className={s.map}>
+            <BahiaBlanca />
+          </div>
         </address>
-       
+
         <address>
           <span>Felix de Azara 1872</span> <br />
           <span>Posadas,</span> <br />
           <span>Misiones</span>, <br />
           <span>3300 -</span> <span> ARG</span>. <br />
           Phone: <span>+54 9 3764 41-0695</span> <br />
-          <div className={s.map}><Posadas/></div>
+          <div className={s.map}>
+            <Posadas />
+          </div>
         </address>
-    
+
         <address>
           <span>Macacha Güemes 515</span> <br />
           <span>Puerto Madero,</span> <br />
           <span>Ciudad Autónoma de Buenos Aires</span>, <br />
           <span>1000-1499 -</span> <span> ARG</span>. <br />
           Phone: <span>+54 9 11 3941-2309</span> <br />
-          <div className={s.map}><PuertoMadero/></div>
+          <div className={s.map}>
+            <PuertoMadero />
+          </div>
         </address>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
