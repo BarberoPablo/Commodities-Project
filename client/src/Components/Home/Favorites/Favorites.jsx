818193b199bf3b9a9800f11bdc5c9cca798a6bc9
Pablo Barbero <pablobarbero220@gmail.com>
2022-09-01 09:35:56 -0300

    Favorites posts, missing back route

--- client/src/Components/Home/Favorites/Favorites.jsx
+++ client/src/Components/Home/Favorites/Favorites.jsx
@@ -1,28 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import s from './Favorites.module.css'
+import React, { useEffect, useState } from "react";
+import s from "./Favorites.module.css";
 
-const Favorites = ({Fav, setFav}) => {
-  
-  const handleDelete = ({e}) =>{
-    const deletePost = Fav.filter(post=> post.id !== e.id)
-    setFav(deletePost)
-  }
+const Favorites = () => {
+  const [favorites, setFavorites] = useState(JSON.parse(window.localStorage.getItem("Fav")));
+
+  const handleDelete = ({ e }) => {
+    const posts = favorites.filter((post) => post.id !== e.id);
+    setFavorites(posts);
+    //No puedo hacer: window.localStorage.setItem("Fav", favorites); porque esta un paso atras y el ultimo no lo elimina
+    window.localStorage.setItem("Fav", JSON.stringify(posts));
+  };
 
   return (
-    <div className={s.container} >
-      {
-        Fav.length > 0 ?
-        Fav?.map((e)=>(
-          <div>
+    <div className={s.container}>
+      {favorites?.length > 0 ? (
+        favorites.map((e, index) => (
+          <div key={index}>
             <h1>{e.title}</h1>
             <p>{e.categoryName}</p>
-            <button onClick={()=>handleDelete({e})}>x</button>
+            <button onClick={() => handleDelete({ e })}>x</button>
           </div>
-        )):
+        ))
+      ) : (
         <h1>You have no favorites</h1>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Favorites
+export default Favorites;
