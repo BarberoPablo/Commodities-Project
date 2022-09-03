be44d8c9b6420ad19501b92c57a5d64a6d3222e0
CesarJelvez <cesarjelvez13@gmail.com>
2022-09-03 18:07:01 -0300

    Feat (api/routes): PUT/plan modify and POST/plan modified

--- api/src/models/Plan.js
+++ api/src/models/Plan.js
@@ -7,7 +7,7 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      primaryKey: true,
+      unique: true,
     },
     cost: {
       type: DataTypes.INTEGER,
