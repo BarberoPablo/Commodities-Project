6680a39320d84520a4c81b6930fd32a0620a17ec
Pablo Barbero <pablobarbero220@gmail.com>
2022-09-06 15:13:47 -0300

    fix: admin panel plans

--- client/src/Redux/Actions/Actions.js
+++ client/src/Redux/Actions/Actions.js
@@ -21,12 +21,10 @@ import { getCountries, sortCountries } from "../Slices/countriesSlice";
 import { getAllPlans } from "../Slices/plansSlice";
 import { getAllReviews } from "../Slices/reviewsSlice";
 
-
 import axios from "axios";
 
-
-// const url = "https://b2b-01.herokuapp.com";
-const url = "http://localhost:3001";
+const url = "https://b2b-01.herokuapp.com";
+//const url = "http://localhost:3001";
 
 // FUNCTIONS POSTS
 
@@ -159,23 +157,27 @@ export const addFavorites = (favorites) => () => {
 };
 
 export const reportTo = (postId, idReview, event) => (dispatch) => {
-  axios.put(`${url}/admin-panel/post/${postId}/${idReview}`, event)
-  .then(() => {dispatch(dispatch(getPost()))
-  })
-  .catch((e) => console.log(e));
+  axios
+    .put(`${url}/admin-panel/post/${postId}/${idReview}`, event)
+    .then(() => {
+      dispatch(dispatch(getPost()));
+    })
+    .catch((e) => console.log(e));
 };
 
 //Review
 
-export const postReview = (review) => () =>{
+export const postReview = (review) => () => {
   axios.post(`${url}/review`, review);
-}
+};
 
-export const banUser = (id) => (dispatch) =>{
-  axios.put(`${url}/userBan/${id}`)
-  .then(() => {dispatch(dispatch(getUser()));
-  })
-  .catch((e) => console.log(e));
+export const banUser = (id) => (dispatch) => {
+  axios
+    .put(`${url}/userBan/${id}`)
+    .then(() => {
+      dispatch(dispatch(getUser()));
+    })
+    .catch((e) => console.log(e));
 };
 
 export const getReviews = (id) => (dispatch) => {
@@ -184,24 +186,33 @@ export const getReviews = (id) => (dispatch) => {
     .catch((e) => console.log(e));
 };
 
-export const deleteReview = (userId, body) => (dispatch) =>{
+export const deleteReview = (userId, body) => (dispatch) => {
   //console.log(body)
-  axios.put(`${url}/admin-panel/review/${userId}`, body)
-  .then(() => {dispatch(getReviews("All"));
-  })
-  .catch((e) => console.log(e));
+  axios
+    .put(`${url}/admin-panel/review/${userId}`, body)
+    .then(() => {
+      dispatch(getReviews("All"));
+    })
+    .catch((e) => console.log(e));
 };
 
-export const updateMembership = (input)=>(dispatch)=>{
-  axios.put(`${url}/plan/${input.nameO}`, input)
-  .then(() => {dispatch(getPlans())
-  })
-  .catch((e) => console.log(e));
-}
+export const updateMembership = (modifyPlan, planName) => (dispatch) => {
+  console.log("ACTION NAME", planName);
+  console.log("ACTION", modifyPlan[planName]);
+  console.log("Ruta", `${url}/plan/` + planName);
+  axios
+    .put(`${url}/plan/` + planName, modifyPlan[planName])
+    .then(() => {
+      dispatch(getPlans());
+    })
+    .catch((e) => console.log(e));
+};
 
-export const createNewPlan = (input)=>(dispatch)=>{
-  axios.post(`${url}/plan/`, input)
-  .then(() => {dispatch(getPlans())
-  })
-  .catch((e) => console.log(e));
-}
+export const createNewPlan = (input) => (dispatch) => {
+  axios
+    .post(`${url}/plan/`, input)
+    .then(() => {
+      dispatch(getPlans());
+    })
+    .catch((e) => console.log(e));
+};
